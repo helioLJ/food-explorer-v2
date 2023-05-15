887e1b752c0e23136b89077556b3754df6666192
Hélio Lúcio <helio.lucio.jr@hotmail.com>
2023-05-15 14:22:10 -0300

    route import

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import { useAuth } from "../hooks/auth";
 
 import { UserRoutes } from "./user.routes";
